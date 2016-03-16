ac1c92511b26d0b915c4f5bf3a21511adf4d5dcc
Elhoucine Azayou <houcineparade@gmail.com>
2016-03-16 22:58:14 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: "elhoucine:clear-subs",
+  summary: "A simple meteor package to clear subscriptions",
+  version: "0.0.1",
+  git: "https://github.com/elhoucine/clear-subs.git"
+});
+
+Package.on_use(function(api) {
+  api.use('1.2');
+
+  api.add_files([
+  ], 'client');
+  
+  api.export('');
+});
+
+//TODO: Tests
+Package.on_test(function(api) {});
